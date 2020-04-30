150a7902cfcef4fbf47e90edd8c805fa9310f6a7
wuchufan <49216918+wuchufan@users.noreply.github.com>
2020-04-30 19:08:18 -0400

    Updates

---
+++ precache-manifest.0bfc2d87697ea3d5dffbe2078bc987a9.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa753aff2d9bdce8d332eba33338b364",
+    "url": "/yuchak-main/index.html"
+  },
+  {
+    "revision": "0ae6dc3e8744424cedee",
+    "url": "/yuchak-main/static/css/2.d609e2c7.chunk.css"
+  },
+  {
+    "revision": "dc21a16c654555b80685",
+    "url": "/yuchak-main/static/css/main.9f1661a9.chunk.css"
+  },
+  {
+    "revision": "0ae6dc3e8744424cedee",
+    "url": "/yuchak-main/static/js/2.8621ac31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yuchak-main/static/js/2.8621ac31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc21a16c654555b80685",
+    "url": "/yuchak-main/static/js/main.0df9b03c.chunk.js"
+  },
+  {
+    "revision": "1680934a45509070a858",
+    "url": "/yuchak-main/static/js/runtime-main.5c990aff.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/yuchak-main/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/yuchak-main/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/yuchak-main/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/yuchak-main/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "233b244a642403d95d88e2b1b0fad732",
+    "url": "/yuchak-main/static/media/article20170119-1.233b244a.jpg"
+  },
+  {
+    "revision": "49ec0f7a02f2808c559de2971e156ebf",
+    "url": "/yuchak-main/static/media/article20170119-2.49ec0f7a.jpg"
+  },
+  {
+    "revision": "abc7fea99092be4351ec0e12f9b093aa",
+    "url": "/yuchak-main/static/media/article20170119-3.abc7fea9.jpg"
+  },
+  {
+    "revision": "6b9674464b34e5c675f5a47993f52845",
+    "url": "/yuchak-main/static/media/article20170202-1.6b967446.jpg"
+  },
+  {
+    "revision": "24b89eca4c8af6d28d56f4ee82a899e5",
+    "url": "/yuchak-main/static/media/article20170202-2.24b89eca.jpg"
+  },
+  {
+    "revision": "d275417fa897bd9ef7edfe5c6ccf45bf",
+    "url": "/yuchak-main/static/media/article20170202-3.d275417f.jpg"
+  },
+  {
+    "revision": "3acc2cf33c2e072e0178b572f122ea1b",
+    "url": "/yuchak-main/static/media/article20170202-4.3acc2cf3.jpg"
+  },
+  {
+    "revision": "6231d0456144cfa0a56149d0a5c770fd",
+    "url": "/yuchak-main/static/media/article20170202-5.6231d045.jpg"
+  },
+  {
+    "revision": "3e2e869b6a444eb4acfe5414a92a8b9f",
+    "url": "/yuchak-main/static/media/building-1.3e2e869b.jpg"
+  },
+  {
+    "revision": "dc9b5b561da6eb336e0bc961838752e6",
+    "url": "/yuchak-main/static/media/building-2.dc9b5b56.jpg"
+  },
+  {
+    "revision": "3e2e869b6a444eb4acfe5414a92a8b9f",
+    "url": "/yuchak-main/static/media/building-3.3e2e869b.jpg"
+  },
+  {
+    "revision": "551ad82eb04c9a37bf46c63c014e043b",
+    "url": "/yuchak-main/static/media/building-4.551ad82e.jpg"
+  },
+  {
+    "revision": "8621e3ff8964eefa328264aa0602cb5e",
+    "url": "/yuchak-main/static/media/building-6.8621e3ff.jpg"
+  },
+  {
+    "revision": "68ad881aa7e94412ebd24d61a7c4fe01",
+    "url": "/yuchak-main/static/media/building-7.68ad881a.jpg"
+  },
+  {
+    "revision": "842c09c3d0b71a10af6c10bdceacb539",
+    "url": "/yuchak-main/static/media/building-8.842c09c3.jpg"
+  },
+  {
+    "revision": "426130011a3027a76cddce31524ef398",
+    "url": "/yuchak-main/static/media/carousel-miami-1.42613001.jpg"
+  },
+  {
+    "revision": "3e2456c8556f8b2eaef8d028955c4a19",
+    "url": "/yuchak-main/static/media/carousel-miami-2.3e2456c8.jpg"
+  },
+  {
+    "revision": "88d844743a9df2cf31508b31f2a63839",
+    "url": "/yuchak-main/static/media/carousel-miami-3.88d84474.jpg"
+  },
+  {
+    "revision": "9ee4b8738f7c29bd2c3650427111bf77",
+    "url": "/yuchak-main/static/media/carousel-miami-4.9ee4b873.jpg"
+  },
+  {
+    "revision": "577300536c423e2a8fea048f0c3dd532",
+    "url": "/yuchak-main/static/media/carousel-orlando-1.57730053.jpg"
+  },
+  {
+    "revision": "4e3ef8c50893c61ea923e3c8da5d9dbb",
+    "url": "/yuchak-main/static/media/carousel-orlando-2.4e3ef8c5.jpg"
+  },
+  {
+    "revision": "809c48eae9960ad8e10c6433534b5d56",
+    "url": "/yuchak-main/static/media/carousel-orlando-3.809c48ea.jpg"
+  },
+  {
+    "revision": "681b3f5f6fd7e0f52086c0ede814bbdc",
+    "url": "/yuchak-main/static/media/landing3.681b3f5f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/yuchak-main/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/yuchak-main/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/yuchak-main/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/yuchak-main/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8fcc9569f3c82133f55f2bacf09de9e3",
+    "url": "/yuchak-main/static/media/startup-landing.8fcc9569.jpg"
+  }
+]);
